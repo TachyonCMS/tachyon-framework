149fe3177f5a45d5a5fb5ccae1ff20c1c9343f72
Brian Winkers <bwinkers@gmail.com>
2021-07-02 19:41:56 -0700

    Cognito Auth working

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <amplify-authenticator username-alias="email">
+    <div id="app">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
+      <amplify-sign-out></amplify-sign-out>
     </div>
-    <router-view/>
-  </div>
+  </amplify-authenticator>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import i18n from './i18n'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
+import '@aws-amplify/ui-vue'
+import Amplify from 'aws-amplify'
+import awsconfig from './aws-exports'
+
+Amplify.configure(awsconfig)
+
 Vue.config.productionTip = false
 
 new Vue({
