16ae28243541aeda8ea740d50ba0d37bd4e6ad05
Brian Winkers <bwinkers@gmail.com>
2021-07-09 01:32:05 -0700

    Initialized Vue/Amplify app with Auth

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <amplify-authenticator username-alias="email">
+      <div>
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <amplify-sign-out></amplify-sign-out>
+      </div>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
